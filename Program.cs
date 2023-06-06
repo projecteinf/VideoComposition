41a108ee04892b025d6a777ee1a9145dc140c701
projecteinf <projecteinf@boscdelacoma.cat>
2023-06-06 13:07:34 -0500

    Llegir fitxers d'un directori

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ internal class Program
         Console.WriteLine("Hello, World!");
         List<model.entitats.File> files = new List<model.entitats.File>();
         files.Add(new model.entitats.File("file1", "path1", 100, 1.1f));
+        String[] filesDirectory = System.IO.Directory.GetFiles(".");
+        foreach (String file in filesDirectory)
+        {
+            Console.WriteLine(file);
+        }
         Console.WriteLine(files[0].ToString());
     }
 }
