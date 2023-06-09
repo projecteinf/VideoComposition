5994567ac8cddfcb090344186fc31ae6c28d51eb
projecteinf <projecteinf@boscdelacoma.cat>
2023-06-09 03:53:09 -0500

    Merge sense audio

--- model/entitats/File.cs
+++ model/entitats/File.cs
@@ -50,18 +50,33 @@ namespace model.entitats
         {
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = "ffmpeg";
-
+        // https://trac.ffmpeg.org/wiki/Concatenate
 /*
+    TOTS ELS VIDEOS EL MATEIX CODEC
 
     ffmpeg -f concat -safe 0 -i /home/miquel/Downloads/myList.txt   -c copy   output.mp4
 
     myList.txt
     file /home/miquel/Projectes/Video/videos/pexels-life-on-super-3209968-1920x1080-18fps.mp4
     file /home/miquel/Projectes/Video/videos/pexels-life-on-super-3209971-1920x1080-18fps.mp4
-*/
 
-            // startInfo.Arguments = $"-i \"concat:{this.Path}|{file.Path}\"  ./videos/merged.mp4";
-            startInfo.Arguments = $"-f concat -safe 0 -i ./videos/myList.txt -c copy ./videos/merged.mp4";
+    // startInfo.Arguments = $"-f concat -safe 0 -i ./videos/myList.txt -c copy ./videos/merged.mp4";
+*/
+/*
+    TOTS ELS VIDEOS AMB DIFERENT CODEC
+
+    ffmpeg -i pexels-life-on-super-3209968-1920x1080-18fps.mp4 \
+        -i pexels-life-on-super-3209971-1920x1080-18fps.mp4 \
+        -filter_complex "[0:v:0][0:a:0][1:v:0][1:a:0]concat=n=2:v=1:a=1[outv][outa]" \
+        -map "[outv]" -map "[outa]" merged.mp4
+
+*/
+            startInfo.Arguments = $" -i  \"{this.Path}\"  "; // 1r video
+            startInfo.Arguments += $" -i  \"{file.Path}\"  "; // 2n video
+            // Console.WriteLine(startInfo.Arguments);
+            startInfo.Arguments += $" -filter_complex \"[0:v:0][1:v:0]"; // stream 1 i 2
+            startInfo.Arguments += $" concat=n=2:v=1[outv]\" "; // concat
+            startInfo.Arguments += $" -map \"[outv]\" ./videos/merged.mp4"; // output
             startInfo.RedirectStandardError = true;
             startInfo.RedirectStandardOutput = true;
             startInfo.UseShellExecute = false;
